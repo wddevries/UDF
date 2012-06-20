d60c7fa43cbfe5edaa3eb54a2c2ed3b41e80f778
Will DeVries <william.devries@gmail.com>
2012-06-19 23:07:25 -0700

    Clean up driver.patch and remove unneeded driver directory.

--- driver/buildscsichanges.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp -r /shared/freebsd/sys/cam/scsi/scsi_cd.c /usr/src/sys/cam/scsi/
-cp -r /shared/freebsd/sys/cam/scsi/scsi_cd.h /usr/src/sys/cam/scsi/
-cp -r /shared/UDF/driver/udfio.h /usr/src/sys/sys/
-cp -r /shared/UDF/driver/udfio.h /usr/include/sys/
